70cb6de3c829c1438621355bce9a52f8dec91f88
vamshireddy24 <88018641+vamshireddy24@users.noreply.github.com>
2024-07-12 17:46:57 +0530

    Update script.js

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,8 +1,5 @@
 // Add JavaScript for the lightbox functionality
-const galleryImages=document.querySelectorAll(".gallery-image"),lightbox=document.querySelector(".lightbox"),lightboxImage=document.createElement("img");galleryImages.forEach(((e,l)=>{e.addEventListener("click",(()=>{lightboxImage.src=e.src,lightbox.appendChild(lightboxImage),document.body.appendChild(lightbox),lightbox.style.display="block"}))})),lightbox.addEventListener("click",(()=>{lightbox.style.display="none",lightboxImage.remove()}));
-
-
-/*const galleryImages = document.querySelectorAll('.gallery-image');
+const galleryImages = document.querySelectorAll('.gallery-image');
 const lightbox = document.querySelector('.lightbox');
 const lightboxImage = document.createElement('img');
 
@@ -15,8 +12,3 @@ galleryImages.forEach((image, index) => {
     });
 });
 
-lightbox.addEventListener('click', () => {
-    lightbox.style.display = 'none';
-    lightboxImage.remove();
-});
-*/
